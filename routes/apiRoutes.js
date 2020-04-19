481d59363f6455003b98c2c17e1db787c179e294
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-04-19 15:39:41 -0400

    tests .env

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 // root directory == "/api/"
 router.get('/', function (req, res, next) {
-  res.json({ question: "Why did the bird go to the doctor?", answer: "For Tweetment!" });
+  res.json({
+    success: true,
+    client_id: process.env.BGA_CLIENT_ID,
+    client_secret: process.env.BGA_CLIENT_SECRET
+  });
 });
 
 module.exports = router;
