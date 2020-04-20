88cddaab482a88876fff0fc82a7d10a41cebdcc4
brittanieb <brittanie.boyko@gmail.com>
2020-04-20 11:11:10 -0400

    implemented basic navigation with react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
+    <Router>
     <div>
-      Game Finder
+      <NavBar />
+      <Route exact path="/" component={Home} />
     </div>
+  </Router>
   );
 }
 
