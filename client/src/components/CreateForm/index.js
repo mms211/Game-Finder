ddc56b8a6081bfa72d336a3b88664e0cf15e309d
brittanieb <brittanie.boyko@gmail.com>
2020-04-20 15:32:50 -0400

    replaced dropdown with radio buttons

--- client/src/components/CreateForm/index.js
+++ client/src/components/CreateForm/index.js
@@ -1,30 +1,49 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 import "./style.css";
 
 function CreateForm() {
   return (
-    <Form>
-      <Form.Group controlId="exampleForm.ControlInput1">
-        <Form.Label>Title</Form.Label>
-        <Form.Control size="lg" type="text" placeholder="Add a Title" />
-      </Form.Group>
-      <Form.Group controlId="exampleForm.ControlInput1">
-        <Form.Label>What Game are you Playing?</Form.Label>
-        <Form.Control type="text" placeholder="Title of Game" />
-      </Form.Group>
-      <Form.Group controlId="exampleForm.ControlSelect1">
-        <Form.Label>Select a Post Type</Form.Label>
-        <Form.Control as="select">
-          <option>Looking for a Player</option>
-          <option>Looking for a Game</option>
-        </Form.Control>
-      </Form.Group>
-      <Form.Group controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Message:</Form.Label>
-        <Form.Control as="textarea" rows="3" />
-      </Form.Group>
-    </Form>
+    <Container>
+        <Form>
+            <Form.Group controlId="exampleForm.ControlInput1">
+                <Form.Label>Title</Form.Label>
+                <Form.Control size="lg" type="text" placeholder="Add a Title" />
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlInput1">
+                <Form.Label>What Game are you Playing?</Form.Label>
+                <Form.Control type="text" placeholder="Title of Game" />
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Message:</Form.Label>
+                <Form.Control as="textarea" rows="3" />
+            </Form.Group>
+            <Form.Group>
+                <Form.Label> Post Type: </Form.Label>
+                <Row>
+                    <Col sm={10}>
+                    <Form.Check
+                        type="radio"
+                        label="Looking for a Player"
+                        name="formHorizontalRadios"
+                        id="formHorizontalRadios1"
+                    />
+                    <Form.Check
+                        type="radio"
+                        label="Looking for a Game"
+                        name="formHorizontalRadios"
+                        id="formHorizontalRadios2"
+                    />
+                    </Col>
+                </Row>
+            </Form.Group>
+            <Button type="submit">Create Post</Button>
+        </Form>
+    </Container>
   );
 }
 
