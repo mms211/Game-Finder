f573b3022eb115c8d87fb38c3d47074869e817ec
brittanieb <brittanie.boyko@gmail.com>
2020-04-20 11:10:42 -0400

    created navbar component and homepage

---
+++ client/src/components/NavBar/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light custom-nav">
+      <Link to="/" className=" navbar-brand text-uppercase">
+        Game Finder
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <Link to="/" className="nav-item nav-link">
+            Make A Post
+          </Link>
+          <Link to="/" className="nav-item nav-link">
+            Filter Posts
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
