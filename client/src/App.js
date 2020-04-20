ef9bb87cbb0fa555765097e4381613fca36ab008
brittanieb <brittanie.boyko@gmail.com>
2020-04-20 11:26:40 -0400

    refactored routing to use Switch case

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./components/pages/Home";
+import NoMatch from "./components/pages/NoMatch";
 
 function App() {
   return (
     <Router>
-    <div>
-      <NavBar />
-      <Route exact path="/" component={Home} />
-    </div>
-  </Router>
+      <div>
+        <NavBar />
+        <Switch>
+          <Route exact path={["/"]}>
+            <Home />
+          </Route>
+          <Route>
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
