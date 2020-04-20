e357b69878b727389208e5cdae8408922e5e7fdf
brittanieb <brittanie.boyko@gmail.com>
2020-04-20 14:01:50 -0400

    refactored nav to use react bootstrap components

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,38 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import "./style.css";
 
 function NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light custom-nav">
-      <Link to="/" className=" navbar-brand text-uppercase">
-        Game Finder
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
-          <Link to="/profile" className="nav-item nav-link">
-            Profile
-          </Link>
-          <Link to="/create" className="nav-item nav-link">
-            Create A Post
-          </Link>
-          <Link to="/filter" className="nav-item nav-link">
-            Filter Posts
-          </Link>
-        </div>
-      </div>
-    </nav>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="/">Game Finder</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/profile">Profile</Nav.Link>
+          <Nav.Link href="/create">Create A Post</Nav.Link>
+          <Nav.Link href="/filter">Filter Posts</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
