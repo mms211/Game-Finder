c8871be1c16f09ee57919cab38b2745da670606c
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-04-22 19:51:33 -0400

    auth

--- bin/isAuthorized.js
+++ bin/isAuthorized.js
@@ -2,7 +2,8 @@ const jwt = require('jsonwebtoken');
 const secret = process.env.JWT_SIGNATURE;
 
 const isAuthorized = (req, res, next) => {
-  const token = req.cookies.token; if (!token) {
+  const token = req.cookies.token;
+  if (!token) {
     res.status(401).send('Unauthorized: No token provided');
   } else {
     jwt.verify(token, secret, (err, decoded) => {
