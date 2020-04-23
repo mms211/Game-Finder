ba0c0597fcab9fca8476e0dd8567c9f975153da3
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-04-23 07:46:47 -0400

    changes router switch for authentication from '/login' to '/signup'

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
           <Route exact path={["/"]}>
             <Home />
           </Route>
-          <Route exact path={["/login"]}>
+          <Route exact path={["/signup"]}>
             <Authentication />
           </Route>
           <Route exact path={["/profile"]}>
