5e5a883aac838409c774cddd1a916167375356bd
brittanieb <brittanie.boyko@gmail.com>
2020-04-24 17:25:55 -0400

    moved delete button to card component

--- client/src/components/Post/index.js
+++ client/src/components/Post/index.js
@@ -1,19 +1,25 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
+import Btn from "../Button";
 import "./style.css";
 
 function Post(props) {
   return (
     <>
-      <Card style={{ width: "18rem" }} className="post-wrapper">
+      <Card className="text-center">
+        <Card.Header>
+          {props.username} {props.createdAt}
+        </Card.Header>
         <Card.Body>
           <Card.Title>{props.title}</Card.Title>
           <Card.Subtitle className="mb-2 text-muted">
-            {props.username}
+            {props.postType}
           </Card.Subtitle>
-          <Card.Subtitle className="mb-2">{props.postType}</Card.Subtitle>
           <Card.Text>{props.body}</Card.Text>
         </Card.Body>
+        <Card.Footer>
+          <Btn variant="dark" title={"Delete"} onClick={props.onClick}></Btn>
+        </Card.Footer>
       </Card>
     </>
   );
