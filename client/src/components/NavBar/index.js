b2a864fc65e141851d5f14c619eb31db0fe046b8
Morgan Sommer <morgana.sommer@gmail.com>
2020-04-26 14:25:05 -0400

    jumbotron added, styles changed on sign in page

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,21 +1,43 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import Jumbotron from 'react-bootstrap/Jumbotron';
 import "./style.css";
 
 function NavBar() {
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="/">Game Finder</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="/profile">Profile</Nav.Link>
-          <Nav.Link href="/create">Create A Post</Nav.Link>
-          <Nav.Link href="/filter">Filter Posts</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <>
+      <Jumbotron fluid className="jumbo">
+        <h1>Game Finder</h1>
+      </Jumbotron>
+      <Nav className="justify-content-center navbar" activeKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/" className="navFont">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/profile" className="navFont">Profile</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/create" className="navFont">Create a Post</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/filter" className="navFont">Filter Posts</Nav.Link>
+        </Nav.Item>
+      </Nav>
+
+
+      {/* <Navbar expand="lg" className='navbar d-flex justify-content-center'>
+        <Navbar.Brand href="/" className="navFont logo">Home</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/profile" className="navFont">Profile</Nav.Link>
+            <Nav.Link href="/create" className="navFont">Create A Post</Nav.Link>
+            <Nav.Link href="/filter" className="navFont">Filter Posts</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar> */}
+    </>
   );
 }
 
