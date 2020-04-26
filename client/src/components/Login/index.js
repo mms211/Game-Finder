dcaf3a08068e15d37a77cbc76c0723e069a3ccf5
Morgan Sommer <morgana.sommer@gmail.com>
2020-04-26 11:28:38 -0400

    some css

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,37 +1,44 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import "./style.css";
 
 function Login(props) {
   return (
-    <Form>
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control
-          type="email"
-          name="email"
-          placeholder="Enter email"
-          autoComplete="email"
-          onChange={props.handleInputChange}
-          required
-        />
-      </Form.Group>
+    <Container fluid className='loginContainer'> 
+      <Card body className='loginCard'>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              name="email"
+              placeholder="Enter email"
+              autoComplete="email"
+              onChange={props.handleInputChange}
+              required
+            />
+          </Form.Group>
 
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          type="password"
-          name="password"
-          placeholder="Password"
-          autoComplete="password"
-          onChange={props.handleInputChange}
-          required
-        />
-      </Form.Group>
-      <Button variant="dark" type="submit" onClick={props.onSubmit}>
-        Submit
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              name="password"
+              placeholder="Password"
+              autoComplete="password"
+              onChange={props.handleInputChange}
+              required
+            />
+          </Form.Group>
+          <Button variant="dark" type="submit" onClick={props.onSubmit}>
+            Submit
       </Button>
-    </Form>
+        </Form>
+      </Card>
+    </Container>
   );
 }
 
