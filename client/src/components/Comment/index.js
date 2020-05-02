9f2d6453f0f9462ef1ee4291bbf757749e04dac0
brittanieb <brittanie.boyko@gmail.com>
2020-05-02 12:44:53 -0400

    created commentForm component for user input

--- client/src/components/Comment/index.js
+++ client/src/components/Comment/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 
-function Comment(props) {
+const Comment = (props) => {
     return (
         <Card.Body>
             <ListGroup variant="flush">
---
+++ client/src/components/CommentForm/index.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+const CommentForm = (props) => {
+
+  return (
+    <Form>
+      <Form.Group controlId="exampleForm.ControlTextarea1">
+        <Form.Control
+          as="textarea"
+          rows="3"
+          placeholder="Add a comment..."
+          onChange={props.onChange}
+        />
+        <Button variant="dark" type="submit" onClick={props.onClick}>
+          Submit
+        </Button>
+      </Form.Group>
+    </Form>
+  );
+}
+export default CommentForm;
