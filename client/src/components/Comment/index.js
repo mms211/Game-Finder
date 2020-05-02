aee70e3f8d26d136f7ad69f0f097d81d4ed3d55b
brittanieb <brittanie.boyko@gmail.com>
2020-05-02 12:38:28 -0400

    created Comment component

--- client/src/components/Comment/index.js
+++ client/src/components/Comment/index.js
@@ -1,50 +1,15 @@
-import React, { useState } from "react";
-import Modal from "react-bootstrap/Modal";
-import Button from "react-bootstrap/Button";
-import Accordion from "react-bootstrap/Accordion";
+import React from 'react';
 import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
 
-function Comment() {
-    const [show, setShow] = useState(false);
-  
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-  
+function Comment(props) {
     return (
-      <>
-        <Button variant="dark" onClick={handleShow}>
-          Leave a Comment
-        </Button>
-  
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Leave A Comment</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-          <Accordion>
-            <Card>
-                <Card.Header>
-                <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                    View All Comments
-                </Accordion.Toggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="0">
-                <Card.Body>Hello! I'm a comment</Card.Body>
-                </Accordion.Collapse>
-            </Card>
-            </Accordion>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </>
+        <Card.Body>
+            <ListGroup variant="flush">
+                <ListGroup.Item>comment goes here</ListGroup.Item>
+            </ListGroup>
+        </Card.Body>
     );
   }
 
-  export default Comment;
+export default Comment;
