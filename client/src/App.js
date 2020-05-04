b73830f3eec4573b7d1640ce53ad4e22099abe3f
brittanieb <brittanie.boyko@gmail.com>
2020-05-04 17:02:10 -0400

    deleted commented out code in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import API from "./utils/API";
 import NavBar from "./components/NavBar";
@@ -13,21 +13,19 @@ import Authentication from "./components/pages/Authentication/Authentication";
 import UserContext from "./utils/UserContext";
 
 const App = () => {
-
   const [userState, setUserState] = useState("ready");
   const [user, setUser] = useState({
     id: "",
-    email: ""
+    email: "",
   });
 
   useEffect(() => {
     setUserState("loading...");
-    API.getCurrentUser()
-      .then(result => {
-        const { id, email } = result.data.userData;
-        setUserState("resolved");
-        setUser({ id, email });
-      });
+    API.getCurrentUser().then((result) => {
+      const { id, email } = result.data.userData;
+      setUserState("resolved");
+      setUser({ id, email });
+    });
   }, []);
 
   return (
@@ -41,9 +39,6 @@ const App = () => {
           <Route exact path="/login">
             <Login />
           </Route>
-          {/* <Route exact path="/login">
-            <BundtCake setUser={setUser} />
-          </Route> */}
           <PrivateRoute exact path="/">
             <Home />
           </PrivateRoute>
@@ -63,6 +58,6 @@ const App = () => {
       </Router>
     </UserContext.Provider>
   );
-}
+};
 
 export default App;
