7037aa88490b34c6f6632792c8457e82b08b3de6
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-05-06 15:05:39 -0400

    re-styles post creation form

--- client/src/components/CreateForm/index.js
+++ client/src/components/CreateForm/index.js
@@ -10,41 +10,41 @@ import "./style.css";
 function CreateForm(props) {
   return (
     <div className="createForm">
-    <Container className="contForm">
-      <Form>
-        <Form.Group controlId="exampleForm.ControlInput1">
-          <Form.Label>Title</Form.Label>
-          <Form.Control size="lg" type="text" placeholder="Title your post" name="title" onChange={props.handleInputChange} required />
-        </Form.Group>
-        <Form.Group controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Message:</Form.Label>
-          <Form.Control as="textarea" rows="3" name="body" onChange={props.handleInputChange} required />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label> Post Type: </Form.Label>
-          <Row>
-            <Col sm={10} onChange={props.handleInputChange}>
-              <Form.Check
-                type="radio"
-                label="Looking for a player"
-                name="postType"
-                id="formHorizontalRadios1"
-                value="Looking for a player"
-              />
-              <Form.Check
-                type="radio"
-                label="Looking for a game"
-                name="postType"
-                id="formHorizontalRadios2"
-                value="Looking for a Game"
-              />
-            </Col>
-          </Row>
-        </Form.Group>
-        <GameSearchForm />
-        <Button type="submit" disabled={props.disabled} onClick={props.handleFormSubmit}>Create Post</Button>
-      </Form>
-    </Container>
+      <Container className="contForm">
+        <Form>
+          <Form.Group controlId="exampleForm.ControlInput1">
+            <Form.Label>Title</Form.Label>
+            <Form.Control size="lg" type="text" placeholder="Title your post" name="title" onChange={props.handleInputChange} required />
+          </Form.Group>
+          <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Message:</Form.Label>
+            <Form.Control as="textarea" rows="3" name="body" onChange={props.handleInputChange} required />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label> Post Type: </Form.Label>
+            <Row>
+              <Col sm={10} onChange={props.handleInputChange}>
+                <Form.Check
+                  type="radio"
+                  label="Looking for a player"
+                  name="postType"
+                  id="formHorizontalRadios1"
+                  value="Looking for a player"
+                />
+                <Form.Check
+                  type="radio"
+                  label="Looking for a game"
+                  name="postType"
+                  id="formHorizontalRadios2"
+                  value="Looking for a Game"
+                />
+              </Col>
+            </Row>
+          </Form.Group>
+          <GameSearchForm />
+          <Button className="form-submit-button" type="submit" disabled={props.disabled} onClick={props.handleFormSubmit}>Create Post</Button>
+        </Form>
+      </Container>
     </div>
   );
 }
