4346c5c318df628a16574d878e4d6d00a78973dc
brittanieb <brittanie.boyko@gmail.com>
2020-05-06 17:38:22 -0400

    reconfigured the layout of the post component

--- client/src/components/Post/index.js
+++ client/src/components/Post/index.js
@@ -9,36 +9,36 @@ function Post(props) {
   const dateToFormat = props.createdAt;
 
   return (
-    <>
-      <Card className="text-center post-wrapper" border="secondary">
-        <Card.Header>
-          {props.username}
-          <Moment fromNow ago>
-            {dateToFormat}
-          </Moment>{" "}
-          ago
-        </Card.Header>
-        <Card.Body>
-          <Image
-            src={
-              props.postType === "Looking for a player"
-                ? require("../../assets/images/player.png")
-                : require("../../assets/images/game.png")
-            }
-            className="post-icon"
-          />
-          <Card.Title>{props.title}</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            {props.postType}
-          </Card.Subtitle>
-          <Card.Text>{props.body}</Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          {props.children}
-          <CommentList postId={props.id} />
-        </Card.Footer>
-      </Card>
-    </>
+    <Card className="post-wrapper">
+      <Card.Header>
+        <Image
+          src={
+            props.postType === "Looking for a player"
+              ? require("../../assets/images/player.png")
+              : require("../../assets/images/game.png")
+          }
+          className="post-icon"
+        />{" "}
+        {props.title}
+      </Card.Header>
+      <Card.Body>
+        <Card.Text>{props.body}</Card.Text>
+        <Card.Text>
+          <small className="text-muted">
+            {props.username} {"- "}
+            Posted{" "}
+            <Moment fromNow ago>
+              {dateToFormat}
+            </Moment>{" "}
+            ago{" "}
+          </small>
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer>
+        {props.children}
+        <CommentList postId={props.id} />
+      </Card.Footer>
+    </Card>
   );
 }
 
