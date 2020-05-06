cc8005bc0b47d4e05c60d4c9d309f684473abe04
Morgan Sommer <morgana.sommer@gmail.com>
2020-05-06 16:44:41 -0400

    removed filter from navbar

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -12,7 +12,6 @@ function NavBar() {
           <Nav.Link href="/" className="navFont">Home</Nav.Link>
           <Nav.Link href="/profile" className="navFont">Profile</Nav.Link>
           <Nav.Link href="/create" className="navFont">Create a Post</Nav.Link>
-          <Nav.Link href="/filter" className="navFont">Filter Posts</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
