9f7ca3b070fb4731f5154b502fc8a1f5532e23ad
brittanieb <brittanie.boyko@gmail.com>
2020-05-06 16:06:48 -0400

    conditionally render player or game icon on each post

--- client/src/components/Post/index.js
+++ client/src/components/Post/index.js
@@ -1,21 +1,31 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import Btn from "../Button";
+import Image from "react-bootstrap/Image";
 import "./style.css";
-import Moment from 'react-moment';
-import CommentList from '../CommentList';
+import Moment from "react-moment";
+import CommentList from "../CommentList";
 
 function Post(props) {
-  const dateToFormat = props.createdAt
+  const dateToFormat = props.createdAt;
+
   return (
     <>
       <Card className="text-center post-wrapper" border="secondary">
         <Card.Header>
-          {props.username}{" "}
-          <Moment fromNow ago>{dateToFormat}</Moment>
-          {" "}ago 
+          <Moment fromNow ago>
+            {dateToFormat}
+          </Moment>{" "}
+          ago
         </Card.Header>
         <Card.Body>
+          <Image
+            src={
+              props.postType === "Looking for a player"
+                ? require("../../assets/images/player.png")
+                : require("../../assets/images/game.png")
+            }
+            className="post-icon"
+          />
           <Card.Title>{props.title}</Card.Title>
           <Card.Subtitle className="mb-2 text-muted">
             {props.postType}
@@ -24,7 +34,7 @@ function Post(props) {
         </Card.Body>
         <Card.Footer>
           {props.children}
-          <CommentList postId={props.id}/>
+          <CommentList postId={props.id} />
         </Card.Footer>
       </Card>
     </>
